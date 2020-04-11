2825638e323d1ae5dafa24c84a0be79fc97a5083
Johannes Kern <johannes.kern@zoho.com>
2020-04-11 14:55:32 +0200

    Added zmq with pub-sub

--- zmq/main.cpp
+++ zmq/main.cpp
@@ -3,9 +3,26 @@
 
 int main()
 {
-	zmq::context_t ctx;
-	zmq::socket_t sock(ctx, zmq::socket_type::push);
-	sock.bind("inproc://test");
-	const std::string_view m = "Hello, world";
-	sock.send(zmq::buffer(m), zmq::send_flags::dontwait);
+	printf("0mq broker started\n");
+	zmq::context_t context_1(1);
+	zmq::context_t context_2(1);
+
+	const char* publishConnection = "tcp://*:5555";  // Sending messages to Subrcibers
+	const char* subcribeConnection = "tcp://*:6000"; // Receiving messages from Publishers
+
+	zmq::socket_t socketPublish(context_1, ZMQ_PUB);
+	socketPublish.bind(publishConnection);
+			     
+	zmq::socket_t sockSubscribe(context_2, ZMQ_SUB);
+	sockSubscribe.bind(subcribeConnection);
+	sockSubscribe.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+			     
+	printf("0mq broker ready\n");
+	zmq::proxy(static_cast<void*>(sockSubscribe), static_cast
+			<void*>(socketPublish), nullptr);
+
+	printf("0mq broker terminated\n");
+
+	return 0;
 }
+
